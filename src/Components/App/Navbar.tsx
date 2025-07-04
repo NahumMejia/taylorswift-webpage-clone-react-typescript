3b598dee2cf3b1f35f5ad5a38201271d14f8117f
NahumMejia <nahummejiad@gmail.com>
2025-07-04 14:56:52 -0600

    Added: Navbar

--- src/Components/App/Navbar.tsx
+++ src/Components/App/Navbar.tsx
@@ -1,14 +1,25 @@
-import TayloSwiftLogo from '../../assets/TsLogo.png';
+import TaylorSwiftLogo from '../../assets/TsLogo.png';
 
-function NavBar(){
-    return(
-        <header className="flex justify-between items-center bg-mainColor px-4 w-full h-16">
-                <img 
-                    className=""
-                    id="TaylorLogo"
-                    src={TayloSwiftLogo}>
-                </img>
+function NavBar() {
+    return (
+        <header className="flex items-center mt-1 ml-2 px-6 w-full h-20.5">
+            <img
+                className="pt-3 h-full max-h-full"
+                id="TaylorLogo"
+                src={TaylorSwiftLogo}
+                alt="Taylor Swift Logo"
+            />
+            <hr className="mt-2 ml-4 border-secondaryColor border-t-3 w-[355px]" />
+            <div className='flex space-x-3 mt-2 ml-4 font-taylor text-[28px] text-secondaryColor'>
+                <ol>Shop /</ol>
+                <ol>My Letter /</ol>
+                <ol>Archive /</ol>
+                <ol>Directed Projects /</ol>
+                <ol>Sign Up </ol>
+            </div>
+
+            <hr className="mt-2 ml-4 border-secondaryColor border-t-3 w-[50px]" />
         </header>
-    )
+    );
 }
 export default NavBar;
