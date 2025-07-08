37ef54ec961b99539cf6ecfc9ad2eb1b4650260a
NahumMejia <nahummejiad@gmail.com>
2025-07-08 10:00:33 -0600

    Completed: Navbar.tsx

--- src/Components/App/Navbar.tsx
+++ src/Components/App/Navbar.tsx
@@ -10,12 +10,12 @@ function NavBar() {
                 alt="Taylor Swift Logo"
             />
             <hr className="mt-2 ml-4 border-secondaryColor border-t-3 w-[355px]" />
-            <div className='flex space-x-3 mt-2 ml-4 font-taylor text-[28px] text-secondaryColor'>
+            <div className='flex space-x-4 mt-2 ml-4 font-taylorBold text-[26px] text-secondaryColor'>
                 <ol>Shop /</ol>
                 <ol>My Letter /</ol>
                 <ol>Archive /</ol>
                 <ol>Directed Projects /</ol>
-                <ol>Sign Up </ol>
+                <ol>Sign Up</ol>
             </div>
 
             <hr className="mt-2 ml-4 border-secondaryColor border-t-3 w-[50px]" />
